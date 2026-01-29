90c1aad5de372c2e15a2f34fe81b1814d99beb9d
ChiragSood1 <chiragsood25@gmail.com>
2026-01-29 11:08:39 +0530

    add basic about page

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+export default function AboutPage() {
+  return (
+    <section>
+      <h1>About Me</h1>
+
+      <p style={{ marginTop: "1rem", maxWidth: "60ch" }}>
+        I’m Chirag, a software developer interested in building clean and
+        reliable web applications. I enjoy learning how things work under
+        the hood and improving my skills through hands-on projects.
+      </p>
+
+      <p style={{ marginTop: "1rem", maxWidth: "60ch" }}>
+        This portfolio is a work in progress where I’ll be adding more
+        projects and experiments over time.
+      </p>
+    </section>
+  );
+}
